d49efd69dabe413ae628ca41c0fcdde65aec5f58
unknown <hasib8nh@gmail.com>
2023-04-11 19:41:10 +0600

    add Block component

---
+++ src/components/Block.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div class="block">
+        click me
+    </div>
+</template>
+    
+<script>
+export default{
+    props: ['delay']
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf8f;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
+
+
+
