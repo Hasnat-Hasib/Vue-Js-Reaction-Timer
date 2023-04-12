f7340c47fe20711ed32c0a5200cf9e5bd39c29d7
unknown <hasib8nh@gmail.com>
2023-04-12 17:38:14 +0600

    create startTimer and stopTimer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,22 +1,37 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="block" v-if="showBlock">
+    <div class="block" v-if="showBlock" @click="stopTimer">
         click me
     </div>
 </template>
     
 <script>
+
 export default{
     props: ['delay'],
     data() {
         return {
-            showBlock: false
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
         }
     },
     mounted () {
         setTimeout(() => {
             this.showBlock =true;
+            this.startTimer()
         }, this.delay);
+    },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            console.log(this.reactionTime);
+        }
     }
 
 }
