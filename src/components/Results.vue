87c68a39fac98212fc1c908c457c404cf90a7364
unknown <hasib8nh@gmail.com>
2023-04-13 11:30:05 +0600

    creating Results component

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,1 +1,14 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <p>Reaction time- {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+</script>
+
+<style>
+
+</style>
