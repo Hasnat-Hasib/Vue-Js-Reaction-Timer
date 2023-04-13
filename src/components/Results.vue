adda9a4245274d7e8732fc6594cdcf3c7bc41d3c
unknown <hasib8nh@gmail.com>
2023-04-13 11:41:25 +0600

    add rank options

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,11 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <p>Reaction time- {{ score }} ms</p>
+    <p class="rank"> {{ rank }}</p>
 </template>
 
 <script>
 export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = "Express"
+        } else if (this.score < 400) {
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "Small pace ..."
+        }
+    }
 }
 </script>
 
